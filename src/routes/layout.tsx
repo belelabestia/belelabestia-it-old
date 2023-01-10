5fd294d90627a701f3a0ad26f5044ec106b1efeb
Marco Bellezza <jasone.mb@outlook.com>
2023-01-10 18:07:32 +0100

    wip

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,35 +2,6 @@ import { component$, Slot } from "@builder.io/qwik";
 import { DocumentHead, useNavigate } from "@builder.io/qwik-city";
 import { Overlay } from "~/components/design/overlay";
 
-export default component$(() => {
-  const nav = useNavigate();
-  return (
-    <div>
-      <Slot />
-      <Overlay>
-        <button
-          class="bottom"
-          onClick$={() => {
-            // location.href = "/web";
-            nav.path = "/web";
-          }}
-        >
-          Web
-        </button>
-        <button
-          class="bottom"
-          onClick$={() => {
-            // location.href = "/print";
-            nav.path = "/print";
-          }}
-        >
-          Print
-        </button>
-      </Overlay>
-    </div>
-  );
-});
-
 export const head: DocumentHead = {
   title: "Marco Bellezza - full-stack dev",
   meta: [
@@ -39,4 +10,24 @@ export const head: DocumentHead = {
       content: "Personal and professional site of Marco Bellezza",
     },
   ],
+};
+
+export default component$(() => {
+  const nav = useNavigate();
+
+  return (
+    <div>
+      <Slot />
+      <Overlay>
+        <div class="bottom">
+          <button onClick$={() => navigate(nav, "/web")}>Web</button>
+          <button onClick$={() => navigate(nav, "/print")}>Print</button>
+        </div>
+      </Overlay>
+    </div>
+  );
+});
+
+export const navigate = (nav: ReturnType<typeof useNavigate>, path: string) => {
+  nav.path = path;
 };
