b8600a8f87aa941d59b982dfd58f44462d4d1e53
Marco Bellezza <jasone.mb@outlook.com>
2023-01-13 17:08:39 +0100

    wip

--- src/routes/modular/index.tsx
+++ src/routes/modular/index.tsx
@@ -1,22 +1,10 @@
 import { component$ } from "@builder.io/qwik";
-import Title from "@/content/title.md?inline";
-import Info from "@/content/info.md?inline";
-import AboutMe from "@/content/about-me.md?inline";
-import DeveloperCareer from "@/content/developer-career.mdx?inline";
-import Skills from "@/content/skills.mdx?inline";
-import Channels from "@/content/channels.md?inline";
-import { ProfilePicture } from "~/components/design/profile-picture";
+import CV from "@/content/cv.mdx?inline";
 
 export default component$(() => {
   return (
     <div class="modular">
-      <ProfilePicture />
-      <Title />
-      <Info />
-      <AboutMe />
-      <DeveloperCareer />
-      <Skills />
-      <Channels />
+      <CV />
     </div>
   );
 });
